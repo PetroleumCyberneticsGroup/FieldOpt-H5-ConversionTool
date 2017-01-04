2787c42d86f1af78fdba5feee90921838c84fbbe
bellout <mathias.bellout@ntnu.no>
2017-01-04 10:02:32 +0100

    Set up project, trying boost libs for reading json file (not ready)

---
+++ main.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/detail/json_parser_read.hpp>
+#include <boost/property_tree/detail/json_parser_write.hpp>
+#include <boost/property_tree/detail/json_parser_error.hpp>
+
+
+int main() {
+    using boost::property_tree::ptree;
+
+    boost::property_tree::ptree pt;
+
+    boost::property_tree::json_parser::re
+
+//    boost::property_tree::read_json("data.json", pt);
+
+//    std::cout << "Reading json files with file strings..." << std::endl;
+//    std::ifstream jsonFile;
+//    jsonFile.open()
+//            open("input.txt");
+//
+//    jsonFile = "input.txt";
+//
+//    ptree pt;
+//    read_json(jsonFile, pt);
+//
+//    array_element.
+//
+//    for (auto & array_element: pt) {
+//        for (auto & property: array_element.second) {
+//            std::cout << property.first << " = " << property.second.get_value < std::string > () << "\n";
+//        }
+//    }
+//
+//
+//    return 0;
+}
